859478df293ab5a475d40abb93b6f43b6ce1768d
itsjustonyii <onyinyechiokeke733@gmail.com>
2023-09-09 04:11:54 +0100

    third commit

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ let days = ['sunday', 'monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
 const day = days[ now.getDay() ];
 document.getElementById("day").innerHTML= day;
 
-const hours = now.getHours() -1;
-const hour = hours*3600000
-const min = now.getMinutes()*60000 ;
-const sec = now.getSeconds()*1000;
 
-document.getElementById("time").innerHTML= hour+min+sec;
+const say= now.getTime()
+document.getElementById("time").innerHTML= say;
 }
 
 DateTime();
